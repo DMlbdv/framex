71a13d87f769d37ccecf70d27505faba418ff966
D.Lebedev <lebbeddev@yahoo.com>
2020-02-18 00:02:41 +0300

    init

---
+++ code/canvas.tsx
@@ -0,0 +1,5 @@
+// WARNING: this file is auto generated, any changes will be lost
+import { createDesignComponent, CanvasStore } from "framer"
+const canvas = CanvasStore.shared(); // CANVAS_DATA;
+export const Card = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_hlT23NSSx", {}, 346,158);
+export const Item = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_W9RrWv19b", {}, 347,124);
