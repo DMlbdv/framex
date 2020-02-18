8b52230fd63049d9d824a99f34aaaaf361ae5415
D.Lebedev <lebbeddev@yahoo.com>
2020-02-18 10:50:27 +0300

    init

---
+++ code/Card.tsx
@@ -0,0 +1,50 @@
+import * as React from "react"
+import { Frame, addPropertyControls, ControlType } from "framer"
+import { Edit } from "./canvas";
+
+// Open Preview: Command + P
+// Learn more: https://framer.com/api
+
+export function Card(props) {
+    const { text, tint, ...rest } = props
+
+    return (
+        <Frame
+            {...rest}
+            background={tint}
+            whileHover={{
+                scale: 1.1,
+            }}
+            style={{
+                color: "#fff",
+                fontSize: 16,
+                fontWeight: 600,
+            }}
+        >
+            <Edit 
+                width={525}
+            />
+        </Frame>
+    )
+}
+
+Card.defaultProps = {
+    height: 128,
+    width: 240,
+    text: "Get started!",
+    tint: "#0099ff",
+}
+
+// Learn more: https://framer.com/api/property-controls/
+addPropertyControls(Card, {
+    text: {
+        title: "Text",
+        type: ControlType.String,
+        defaultValue: "Hello Framer!",
+    },
+    tint: {
+        title: "Tint",
+        type: ControlType.Color,
+        defaultValue: "#0099ff",
+    },
+})
--- code/canvas.tsx
+++ code/canvas.tsx
@@ -2,4 +2,5 @@
 import { createDesignComponent, CanvasStore } from "framer"
 const canvas = CanvasStore.shared(); // CANVAS_DATA;
 export const Card = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_hlT23NSSx", {}, 346,158);
+export const Edit = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_kUNN0Vuee", {}, 41,14);
 export const Item = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_W9RrWv19b", {}, 347,124);
